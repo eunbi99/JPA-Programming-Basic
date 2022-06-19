08043420bda14660b99e02d6d52791219b9a878d
eunbi99 <silverbi99@naver.com>
2022-06-19 17:32:14 +0900

    엔티티매핑 -객체와 테이블 매핑/ 필드와 컬럼 매핑

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -14,57 +14,11 @@ public class JpaMain {
         EntityTransaction tx = em.getTransaction();
         tx.begin(); // 데이터베이스 트랜잭션 시작
         try {
-           /* Member 저장
             Member member = new Member();
             member.setId(2L);
-            member.setName("HelloB");
-            em.persist(member);
-           */
-
-            /* Member 조회
-            Member findMember = em.find(Member.class,1L);
-            System.out.println("findMember = " + findMember.getId());
-            System.out.println("findMember = " + findMember.getName());
-            */
-
-            /* Member 삭제
-            Member findMember = em.find(Member.class,1L);
-            em.remove(findMember);
-             */
-
-            /* Member 수정
-            Member findMember = em.find(Member.class,3L);
-            findMember.setName("HelloJPA");
-            */
-
-            /* Member객체를 전부 가져온다.
-           List<Member> result = em.createQuery("select m from Member as m",Member.class)
-                   .setFirstResult(5)
-                   .setMaxResults(8)
-                   .getResultList();
-           for (Member member : result) {
-               System.out.println("member.name" + member.getName());
-           }
-           */
-
-            /*
-            // 비영속
-            Member member = new Member();
-            member.setId(101L); // 1차 캐시에 저장
-            member.setName("Hello JPA!");
-
-            // 영속
-            System.out.println("==== BEFORE ====");
-            em.persist(member);
-            System.out.println("==== AFTER ====");
-            */
-
-            Member findMember1 = em.find(Member.class, 101L);
-            Member findMember2 = em.find(Member.class, 101L); // 영속 컨텍스트에 저장되어있는 1차 캐시를 먼저 확인하고 반환! - select 쿼리 x
-
-            System.out.println("result = " + (findMember1 == findMember2)); // true
-
-            tx.commit();
+            member.setUsername("B");
+            member.setRoleType(RoleType.ADMIN);
+            tx.commit(); // commit 직전 insert 날림.
         } catch (Exception e) {
             tx.rollback();
         } finally {
